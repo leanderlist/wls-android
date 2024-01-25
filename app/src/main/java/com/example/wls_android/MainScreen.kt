506ba121e3b85f5db2e3b50827912c6910a6e47e
Leander List <llist@student.tgm.ac.at>
2024-01-25 12:06:55 +0100

    Add basic UI

---
+++ app/src/main/java/com/example/wls_android/MainScreen.kt
@@ -0,0 +1,40 @@
+package com.example.wls_android
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(text = "WLS")
+                }
+            )
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+                .padding(it)
+        ) {
+
+        }
+    }
+}

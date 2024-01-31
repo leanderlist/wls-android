0daa93eda2c6de8d8519c8a1691bb35b02ac8944
Leander List <llist@student.tgm.ac.at>
2024-01-31 21:44:19 +0100

    Add Details

--- app/src/main/java/com/example/wls_android/MainActivity.kt
+++ app/src/main/java/com/example/wls_android/MainActivity.kt
@@ -8,20 +8,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.compose.WlsAndroidTheme
-import com.example.wls_android.data.Disturbance
-import com.example.wls_android.data.RetrofitHelper
-import com.example.wls_android.data.WlsApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import retrofit2.Response
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             WlsAndroidTheme {
                 Surface(

3849a97dc271c2ed8d78b5e7692a632521c2b46d
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2024-12-12 17:13:25 +0100

    Optimize imports

--- app/src/main/java/com/example/wls_android/composables/WlsHeader.kt
+++ app/src/main/java/com/example/wls_android/composables/WlsHeader.kt
@@ -1,11 +1,8 @@
 package com.example.wls_android.composables
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.SubdirectoryArrowLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,9 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.wls_android.navigation.Screen
 

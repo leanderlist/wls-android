51908edb9cb8cce88f132d6b8e3d0a3d7cd40c8f
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2025-01-09 14:29:19 +0100

    Add splashscreen library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("androidx.core:core-splashscreen:1.2.0-alpha02")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

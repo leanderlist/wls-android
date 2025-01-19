480f8ff951df3935075b27eaa31ebe975285fae6
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2025-01-19 19:35:50 +0100

    Edit target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "at.wls_android.app"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
